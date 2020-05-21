906a956782f56c0f990b21b3e8e091b4647ed5e3
Loganayaki <loganayakir23@gmail.com>
2020-05-21 05:43:43 +0000

    Module 2 Completed

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> 
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-
+        //IdentityDbContext<ApplicationUser>
         public DbSet<Models.Item> Items { get; set; }
     }
 }
