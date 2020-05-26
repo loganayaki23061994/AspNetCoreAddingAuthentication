6ca07ba678fdaf1c648cbb3d5aa339f67e4f9dd1
Loganayaki <loganayakir23@gmail.com>
2020-05-26 11:44:44 +0000

    Module 4 Issue fixed

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -12,13 +12,12 @@ namespace WishList.Models.AccountViewModels
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [StringLength (100)]
-        [MinLength (8)]
+        [StringLength (100, MinimumLength =8)]
         [DataType (DataType.Password)]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
-        [Compare (nameof(Password), ErrorMessage ="Passwords Do not Match")]
+        [Compare ("Password")]
         public string ConfirmPassword { get; set; }
     }
 }
